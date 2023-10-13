3d5f35c0f56a175c0830ef0e39bf70302d7f79df
valeriapadilla <vpadillahoyos@gmail.com>
2023-10-13 17:15:36 -0500

    Actualizacion de program.cs

--- Sistema_buses/Program.cs
+++ Sistema_buses/Program.cs
@@ -1,8 +1,26 @@
+using Sistema_buses.DbContexts;
+using Sistema_buses.Interfaces;
+using Sistema_buses.Repository;
+using Sistema_buses.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Aqui agregamos los servicios requeridos
 
-builder.Services.AddControllers();
+//El DBContext a utilizar
+builder.Services.AddSingleton<PgsqlDbContext>();
+
+//Los repositorios
+builder.Services.AddScoped<ICargadorRepository, CargadorRepository>();
+
+//Aqui agregamos los servicios asociados para cada EndPoint
+builder.Services.AddScoped<CargadorServices>();
+
+// Add services to the container.
+builder.Services.AddControllers()
+    .AddJsonOptions(
+        options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
 
 var app = builder.Build();
 
